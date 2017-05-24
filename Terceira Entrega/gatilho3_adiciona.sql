755560f28a2f50568104d828847cd909d3346cd3
Sofia Silva <up201505628@fe.up.pt>
2017-05-24 15:58:07 +0100

    triggers update

---
+++ Terceira Entrega/gatilho3_adiciona.sql
@@ -0,0 +1,23 @@
+CREATE TRIGGER IF NOT EXISTS nome
+AFTER INSERT ON tabela
+FOR EACH ROW
+WHEN codicao
+  BEGIN
+    INSERT INTO tabela VALUES
+  END;
+
+CREATE TRIGGER IF NOT EXISTS nome
+AFTER DELETE ON tabela
+FOR EACH ROW
+  BEGIN
+    DELETE FROM tabela WHERE algo acontece;
+  END;
+
+CREATE TRIGGER IF NOT EXISTS nome
+AFTER UPDATE OF argumento ON tabela
+FOR EACH ROW
+  BEGIN
+    UPDATE tabela
+    SET argumento = New.argumento
+    WHERE argumento = Old.argumento;
+  END;
