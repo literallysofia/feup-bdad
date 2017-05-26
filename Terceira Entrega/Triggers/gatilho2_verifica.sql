b020bbfafe1a6ee1da6d08c54acf62979961f5e1
Sofia Silva <up201505628@fe.up.pt>
2017-05-26 23:13:30 +0100

    :)

--- Terceira Entrega/Triggers/gatilho2_verifica.sql
+++ Terceira Entrega/Triggers/gatilho2_verifica.sql
@@ -11,19 +11,20 @@ FROM Utilizador
 WHERE Utilizador.id = 1;
 
 .print ''
-.print 'Update da data de nascimento de um Utilizador com ID = 1 -> dispara gatilho no qual é valida ou não a data ;'
+.print 'Update da data de nascimento de um Utilizador com ID = 1 -> dispara gatilho no qual e valida ou nao a data ;'
 .print ''
 .print 'Data de nascimento = 2010-02-20 (caso invalido) :'
 .print ''
 
 UPDATE Utilizador SET dataNascimento = '2010-02-20' WHERE Utilizador.id = 1;
 
+.print ''
 SELECT Utilizador.id AS 'ID', Utilizador.nome AS 'Nome', Utilizador.dataNascimento AS 'Data Nascimento', Utilizador.idade AS 'Idade'
 FROM Utilizador
 WHERE Utilizador.id = 1;
 
 .print ''
-.print 'Ao ser validado, ou seja, apos ser atualizada a data de nascimento, a idade será ajustada.'
+.print 'Ao ser validado, ou seja, apos ser atualizada a data de nascimento, a idade sera ajustada.'
 .print 'Data de nascimento = 1995-02-20 (caso valido) :'
 .print ''
 
