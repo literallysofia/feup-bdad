b257962997880fcf7cb3040f51471c220aa5d21e
Sofia Silva <up201505628@fe.up.pt>
2017-05-26 23:22:51 +0100

    update FINAL de triggers :sun_with_face:

--- Terceira Entrega/Triggers/gatilho3_verifica.sql
+++ Terceira Entrega/Triggers/gatilho3_verifica.sql
@@ -3,15 +3,31 @@
 .nullvalue NULL
 
 .print ''
-.print 'Após criação de MusicaPlaylist -> dispara gatilho no qual incrementa a duração da Playlist ;'
-.print 'Playlist com ID = X ;'
+.print 'Estado atual de Playlist:'
 .print ''
 
-INSERT INTO MusicaPlaylist (idMusica, idPlaylist) VALUES (1, X);
+SELECT Playlist.id AS 'ID', Playlist.nome AS 'Nome', Playlist.duracao AS 'Duracao'
+FROM Playlist
+WHERE Playlist.id = 1;
 
 .print ''
-.print 'Após remoção de MusicaPlaylist -> dispara gatilho no qual decrementa a duração da Playlist ;'
-.print 'Playlist com ID = X ;'
+.print 'Apos criacao de MusicaPlaylist -> dispara gatilho que incrementa a duracao de Playlist ;'
 .print ''
 
-DELETE FROM MusicaPlaylist WHERE MusicaPlaylist.idMusica = 1 AND MusicaPlaylist.idPlaylist = X;
+INSERT INTO MusicaPlaylist (idMusica, idPlaylist) VALUES (1, 1);
+
+SELECT DISTINCT Playlist.id AS 'Playlist ID', Playlist.nome AS 'Playlist nome', Playlist.duracao AS 'Playlist duracao', Musica.id AS 'Musica ID', Musica.duracao AS 'Musica duracao'
+FROM Playlist
+INNER JOIN Musica
+ON Musica.id = 1 AND Playlist.id = 1;
+
+.print ''
+.print 'Apos remocao de MusicaPlaylist -> dispara gatilho que decrementa a duracao de Playlist ;'
+.print ''
+
+DELETE FROM MusicaPlaylist WHERE MusicaPlaylist.idMusica = 1 AND MusicaPlaylist.idPlaylist = 1;
+
+SELECT DISTINCT Playlist.id AS 'Playlist ID', Playlist.nome AS 'Playlist nome', Playlist.duracao AS 'Playlist duracao', Musica.id AS 'Musica ID', Musica.duracao AS 'Musica duracao'
+FROM Playlist
+INNER JOIN Musica
+ON Musica.id = 1 AND Playlist.id = 1;
